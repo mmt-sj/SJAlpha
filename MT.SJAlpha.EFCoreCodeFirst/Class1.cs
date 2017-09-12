80487ccb7e1faf2dd524050c636bede295cd1504
mt <435658081@qq.com>
2017-09-12 21:53:53 +0800

    第一次提交，功能  前台申请加入 后台用户管理

---
+++ MT.SJAlpha.EFCoreCodeFirst/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace MT.SJAlpha.EFCoreCodeFirst
+{
+    public class Class1:DbContext
+    {
+    }
+}
---
+++ MT.SJAlpha.EFCoreCodeFirst/Entitis/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace MT.SJAlpha.EFCoreCodeFirst.Entitis
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(256),Required]
+        public string Name { get; set; }
+    }
+}
