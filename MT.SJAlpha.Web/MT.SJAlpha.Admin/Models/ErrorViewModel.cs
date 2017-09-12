13c6e903a6b59668034e9088d0bf704194e5a11c
mt <435658081@qq.com>
2017-09-12 22:20:21 +0800

    第一次  后台：招新  前台 纳新

---
+++ MT.SJAlpha.Web/MT.SJAlpha.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MT.SJAlpha.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MT.SJAlpha.Web/MT.SJAlpha.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MT.SJAlpha.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
